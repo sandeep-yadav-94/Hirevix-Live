4e354fe54531f5f8da35fa94c89ff2aabd7e2089
sandeep-yadav <sandeepyadavtheking@gmail.com>
2026-08-15 09:26:26 -0700

    cors seted

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOption = {
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'https://hirevix-live.vercel.app',
     credentials: true,
 };
 
